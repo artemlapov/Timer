e51170849bb0cbf480439c90b62c8a80a0b0c7e5
Artem LAPOV <artemlapov@me.com>
2023-08-31 11:27:09 +0200

    Initial Commit

---
+++ Timer/TimerApp.swift
@@ -0,0 +1,17 @@
+//
+//  TimerApp.swift
+//  Timer
+//
+//  Created by Artem Lapov on 31/08/2023.
+//
+
+import SwiftUI
+
+@main
+struct TimerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
