1f3f0a8ced45d2371099a4ccec6c9d91c5d8cead
Artem LAPOV <artemlapov@me.com>
2023-09-01 09:52:32 +0200

    The user interface has been created

--- Timer/ContentView.swift
+++ Timer/ContentView.swift
@@ -8,14 +8,52 @@
 import SwiftUI
 
 struct ContentView: View {
-    var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+
+
+    let defaultTime: CGFloat = 20
+
+    @State private var timerRunning = false
+    @State private var countdownTime: CGFloat = 20
+
+    let timer = Timer.publish(every: 1, on: .main, in: .common).autoconnect()
+    var countdownColor: Color {
+        switch countdownTime {
+        case 6...: return Color.green
+        case 3...: return Color.yellow
+        default: return Color.red
         }
-        .padding()
+    }
+
+    let strokeStyle = StrokeStyle(lineWidth: 15, lineCap: .round)
+   // let buttonIcon = timerRunning ? "pause.rectangle.fill" : "play.rectangle.fill"
+    let buttonIcon = "play.rectangle.fill"
+
+    var body: some View {
+        ZStack {
+            Circle()
+                .stroke(.gray.opacity(0.2), style: strokeStyle)
+            Circle()
+                .trim(from: 0, to: 1 - ((defaultTime - countdownTime) / defaultTime))
+                .stroke(countdownColor, style: strokeStyle)
+                .rotationEffect(.degrees(-90))
+            .animation(.easeInOut)
+            HStack(spacing: 25) {
+                Label("", systemImage: buttonIcon)
+                    .foregroundColor(.black).font(.title)
+                    .onTapGesture {
+                        timerRunning.toggle()
+                    }
+                Text("\(Int(countdownTime))")
+                    .font(.largeTitle)
+                Label("", systemImage: "gobackward")
+                    .foregroundColor(.red)
+                    .font(.title)
+                    .onTapGesture {
+                        timerRunning = false
+                        countdownTime = defaultTime
+                    }
+            }
+        }
     }
 }
 
